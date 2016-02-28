8182fcecdd714d591e10586ddbcd5e57f4924435
marcgille <latakia42>
2016-02-27 21:54:48 -0500

    Command classes for Thermostat.

--- default-units/thermostatGeneric.js
+++ default-units/thermostatGeneric.js
@@ -6,13 +6,37 @@ module.exports = {
         family: "thermostatGeneric",
         deviceTypes: ["z-wave/zWaveNetwork"],
         services: [],
-        state: [
-            {
-                id: "value", label: "Value",
-                type: {
-                    id: "integer"
-                }
-            }],
+        state: [{
+            id: "mode", label: "Mode",
+            type: {
+                id: "string"
+            }
+        }, {
+            id: "operatingState", label: "Operating State",
+            type: {
+                id: "string"
+            }
+        }, {
+            id: "setpoint", label: "Setpoint",
+            type: {
+                id: "decimal"
+            }
+        }, {
+            id: "temperature", label: "Temperature",
+            type: {
+                id: "decimal"
+            }
+        }, {
+            id: "fanMode", label: "Fan Mode",
+            type: {
+                id: "string"
+            }
+        }, {
+            id: "fanState", label: "Fan State",
+            type: {
+                id: "string"
+            }
+        }],
         configuration: [{
             label: "Node ID",
             id: "nodeId",
@@ -60,16 +84,54 @@ function ThermostatGeneric() {
         if (comClass == 32) { // Basic
             this.publishStateChange();
         }
-        else if (comClass == 48) { // SensorBinary
-            this.logDebug("Level of Binary Sensor", command);
+        else if (comClass == 49) { // Multi Sensor
+            this.logDebug("Sensor Value", command);
+            this.state.temperature = command.value;
 
-            this.state.value = command.value ? 100 : 0;
             this.publishStateChange();
+        }
+        else if (comClass == 64) { // Thermostat Mode
+            this.logDebug("Thermostat Mode", command);
+            this.state.mode = command.value;
+
+            this.publishStateChange();
+        }
+        else if (comClass == 66) { // Thermostat Operating State
+            this.logDebug("Thermostat Operating State", command);
+            this.state.operatingState = command.value;
+
+            this.publishStateChange();
+        }
+
+        else if (comClass == 67) { // Thermostat Setpoint
+            this.logDebug("Thermostat Setpoint", command);
+            this.state.setpoint = command.value;
+
+            this.publishStateChange();
+        }
+        else if (comClass == 68) { // Thermostat Fan Mode
+            this.logDebug("Thermostat Fan Mode", command);
+            this.state.fanMode = command.value;
+
+            this.publishStateChange();
+        }
+        else if (comClass == 69) { // Thermostat Fan State
+            this.logDebug("Thermostat Fan State", command);
+
+            this.state.fanState = command.value;
+            this.publishStateChange();
+        }
+        else if (comClass == 129) { // Clock
+            this.logDebug("Clock", command.value);
         }
         else if (comClass == 132) { // Wakeup
             this.logDebug("Wakeup", command.value);
-
-            this.publishStateChange();
+        }
+        else if (comClass == 134) { // Version
+            this.logDebug("Version", command.value);
+        }
+        else if (comClass == 135) { // Indicator
+            this.logDebug("Indicator", command.value);
         }
         else {
             this.logDebug("Other State Change", comClass, command.value);
@@ -79,14 +141,14 @@ function ThermostatGeneric() {
     /**
      *
      */
-    ThermostatGeneric.prototype.handleEventFromZWave = function(event, valueid) {
+    ThermostatGeneric.prototype.handleEventFromZWave = function (event, valueid) {
         this.logDebug("Event: " + event + " on Value ID " + valueid);
     }
 
     /**
      *
      */
-    ThermostatGeneric.prototype.handleNotificationFromZWave = function(notif, help) {
+    ThermostatGeneric.prototype.handleNotificationFromZWave = function (notif, help) {
         this.logDebug(help + " (" + notif + ")");
     }
 
